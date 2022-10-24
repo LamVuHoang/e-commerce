0d3253559012bafaf3f79bccc0e9aef004c35f6d
Lam Vu Hoang <lam.vuhoang@nashtechglobal.com>
2022-10-24 17:26:04 +0700

    feat(fe) Home Carousel

--- resources/js/Components/Home/Banner/index.jsx
+++ resources/js/Components/Home/Banner/index.jsx
@@ -38,106 +38,120 @@ function index() {
 
     return (
         <>
-            {bannerList != null && (
-                <div className="w-full mb-5 md:mb-3 flex relative">
-                    {bannerList.map((item, index) => {
-                        return (
-                            <a
-                                key={index}
-                                href={item.link}
-                                className={`w-full shrink-0 slider-item ${
-                                    index < current
-                                        ? "slider-item--before"
-                                        : index > current
-                                        ? "slider-item--after"
-                                        : "slider-item--current"
-                                }`}
-                            >
-                                {width >= 1024 ? (
-                                    <img
-                                        key={index}
-                                        src={item.desktop_image}
-                                        alt={item.alt}
-                                        className="w-full object-cover rounded-2xl"
-                                        onError={({ currentTarget }) => {
-                                            currentTarget.onerror = null; // prevents looping
-                                            currentTarget.src =
-                                                "https://via.placeholder.com/1600x400?text=No+Image";
-                                        }}
-                                    />
-                                ) : (
-                                    <img
-                                        key={index}
-                                        src={item.mobile_image}
-                                        alt={item.alt}
-                                        className="w-full object-cover rounded-2xl"
-                                        onError={({ currentTarget }) => {
-                                            currentTarget.onerror = null; // prevents looping
-                                            currentTarget.src =
-                                                "https://via.placeholder.com/1600x900?text=No+Image";
-                                        }}
-                                    />
-                                )}
-                            </a>
-                        );
-                    })}
-                    <button
-                        className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center absolute left-2 top-1/2 -translate-y-1/2 disabled:text-gray-200"
-                        onClick={prevButtonHandler}
-                        disabled={current == 0}
+            <div id="default-carousel" class="relative" data-carousel="static">
+                {/*  Carousel wrapper  */}
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    {/*  Item 1  */}
+                    <div
+                        class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
+                        data-carousel-item=""
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M15 19l-7-7 7-7"
-                            />
-                        </svg>
-                    </button>
-                    <button
-                        className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center absolute right-2 top-1/2 -translate-y-1/2 disabled:text-gray-200"
-                        onClick={nextButtonHandler}
-                        disabled={current == bannerList.length - 1}
+                        <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">
+                            First Slide
+                        </span>
+                        <img
+                            src="https://i.ebayimg.com/images/g/1kYAAOSwUG9jVgMP/s-l1600.webp"
+                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            alt="..."
+                        />
+                    </div>
+                    {/*  Item 2  */}
+                    <div
+                        class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-full z-10"
+                        data-carousel-item=""
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M9 5l7 7-7 7"
-                            />
-                        </svg>
-                    </button>
-                    <div className="flex absolute top-full left-1/2 mt-2 -translate-x-1/2">
-                        {bannerList.map((item, index) => {
-                            return (
-                                <button
-                                    className={`h-2 mx-1 bg-gray-300 rounded-full transition-all ${
-                                        current === index
-                                            ? "bg-gray-600 hover:bg-gray-700 w-8"
-                                            : "hover:bg-gray-400 w-5"
-                                    }`}
-                                    onClick={() => selectButtonHandler(index)}
-                                    key={index}
-                                />
-                            );
-                        })}
+                        <img
+                            src="https://i.ebayimg.com/images/g/clgAAOSwVgNjT8Q7/s-l1600.webp"
+                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            alt="..."
+                        />
+                    </div>
+                    {/*  Item 3  */}
+                    <div
+                        class="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10"
+                        data-carousel-item=""
+                    >
+                        <img
+                            src="https://i.ebayimg.com/images/g/df8AAOSwuSxjRN9S/s-l1600.webp"
+                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            alt="..."
+                        />
                     </div>
                 </div>
-            )}
+                {/*  Slider indicators  */}
+                <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                    <button
+                        type="button"
+                        class="w-3 h-3 rounded-full bg-white dark:bg-gray-800"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                        data-carousel-slide-to="0"
+                    ></button>
+                    <button
+                        type="button"
+                        class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
+                        aria-current="false"
+                        aria-label="Slide 2"
+                        data-carousel-slide-to="1"
+                    ></button>
+                    <button
+                        type="button"
+                        class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
+                        aria-current="false"
+                        aria-label="Slide 3"
+                        data-carousel-slide-to="2"
+                    ></button>
+                </div>
+                {/* Slider controls  */}
+                <button
+                    type="button"
+                    class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    data-carousel-prev=""
+                >
+                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M15 19l-7-7 7-7"
+                            ></path>
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button
+                    type="button"
+                    class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                    data-carousel-next=""
+                >
+                    <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9 5l7 7-7 7"
+                            ></path>
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
+            </div>
         </>
     );
 }
