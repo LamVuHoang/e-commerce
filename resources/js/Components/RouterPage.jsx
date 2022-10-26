ade7e887efb7faacd10f91dc92daf4cc55dfebc2
Lam Vu Hoang <lam.vuhoang@nashtechglobal.com>
2022-10-26 10:28:56 +0700

    fix(fe) Link and Router

--- resources/js/Components/RouterPage.jsx
+++ resources/js/Components/RouterPage.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home/index";
 import Header from "./Header/index";
 import Footer from "./Footer/index";
+import Home from "./Home/index";
+import Test from "./Test/index";
 
 export default function RouterPage() {
     return (
         <>
-            <Header />
             <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/test" element={<Test />} />
                 </Routes>
+                <Footer />
             </BrowserRouter>
-            <Footer />
         </>
     );
 }
