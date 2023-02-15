64a735d672937e79c857f59a24877e39f53551b4
Lam Vu Hoang <lam.vuhoang@nashtechglobal.com>
2023-02-15 15:52:31 +0700

    feat(be) Laravel sail

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.tsx'],
+            input: ["resources/scss/app.scss", "resources/js/app.tsx"],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
